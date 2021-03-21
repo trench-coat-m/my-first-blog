31cde83cb2cfdd20f64321d18ab1e9e17a4bce55
utsuminaoto <naotou1012@icloud.com>
2021-03-21 19:58:56 +0900

    added HTML

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'xw7o1us8#2!fa!0sm!%ozaxpeh!s%htzbd87#$)d5**vx^wn1d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
